976df01cfd6cdb8e2759600605dc478e28697562
data0x1 <79959516+data0x1@users.noreply.github.com>
2021-03-10 16:46:05 +0900

    자료구조2주차

---
+++ week2/src/OneDim.java
@@ -0,0 +1,8 @@
+public class OneDim {
+	public static void main(String[] args) {
+		int sale[] = new int[] {157, 209, 251, 312};
+		
+		for(int i=0; i <4; i++)
+			System.out.println(sale[i]);
+	}
+}
---
+++ week2/src/TwoDim.java
@@ -0,0 +1,13 @@
+
+public class TwoDim {
+
+	public static void main(String[] args) {
+		int sale[][] = new int[][] {{63, 84, 140, 130},
+				{157, 209, 251, 312}};
+
+				for(int i=0; i<2; i++)
+					for(int j=0; j<4; j++)
+						System.out.println(sale[i][j]);
+	}
+
+}
